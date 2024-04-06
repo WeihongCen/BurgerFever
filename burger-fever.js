bc822e298dd126fa7aa4766e1f6f2002a8e4140e
William <21cenweihong@gmail.com>
2024-04-06 15:50:23 -0700

    Finished main menu

---
+++ burger-fever.js
@@ -0,0 +1,15 @@
+/**
+ * @author Weihong Cen
+ * @date 05/13/2023
+ * 
+ * This is the JavaScript that manages the game and card states in the SETS! 
+ * website.
+ */
+(function() {
+    "use strict";
+
+    function init() {
+    }
+    
+    init();
+})();
