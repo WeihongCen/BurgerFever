e08229a5adb6be80fa65451e1870483364ef4aef
William <21cenweihong@gmail.com>
2024-04-09 15:58:25 -0700

    combine to singular page

--- burger-fever.js
+++ burger-fever.js
@@ -8,12 +8,17 @@
 (function() {
     "use strict";
 
+    const INGREDIENTS = ["bottom-bun", "cheese", "lettuce", "onion", "patty", 
+        "pickles", "tomato", "top-bun"];
+
     function init() {
         const START_BUTTON = qs("#start-btn");
         START_BUTTON.addEventListener("click", start);
+        populateCards();
     }
     
     function start() {
+        populateCards();
         toggleView();
     }
 
@@ -27,5 +32,29 @@
         GAME_VIEW.classList.toggle("hidden");
     }
     
+    function populateCards() {
+        let ingredientsList = qs("#ingredient-list");
+        for (let i = 0; i < INGREDIENTS.length; i++) {
+            var ingredientCard = document.createElement("div");
+            ingredientCard.classList.add("ingredient-card");
+
+            var ingredientName = document.createElement("strong");
+            ingredientName.textContent = INGREDIENTS[i];
+
+            var imgContainer = document.createElement("div");
+            imgContainer.classList.add("img-container");
+
+            var ingredientImg = document.createElement("img");
+            ingredientImg.classList.add("ingredients");
+            ingredientImg.src = `imgs/${INGREDIENTS[i]}.png`;
+            ingredientImg.alt = INGREDIENTS[i];
+
+            ingredientCard.appendChild(ingredientName);
+            imgContainer.appendChild(ingredientImg);
+            ingredientCard.appendChild(imgContainer);
+            ingredientsList.appendChild(ingredientCard);
+        }
+    }
+
     init();
 })();
